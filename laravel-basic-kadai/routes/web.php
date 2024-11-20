e9175a675fb53a0a3c4e00f2282ea60c8599e2b3
ISSEI-dot <g27e4e8bdeypv@gmail.com>
2024-11-20 14:12:02 +0900

    Laravel-Breeze認証

--- laravel-basic-kadai/routes/web.php
+++ laravel-basic-kadai/routes/web.php
@@ -1,8 +1,10 @@
 <?php
-use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
+Route::resource('posts', PostController::class);
 
 /*
 |--------------------------------------------------------------------------
@@ -13,16 +15,32 @@ use App\Http\Controllers\PostController;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*///
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::get('/post', function () {
+    return 'Hello, world!';
+});
+
 Route::get('/posts', [PostController::class, 'index']);
 
-Route::get('/posts/create',[PostController::class,'create']);
+Route::get('/posts/create',[PostController::class,'create'])->middleware('auth');
 
-Route::post('/posts/store',[PostController::class,'store'])->name('posts.store');
+Route::post('/posts/store',[PostController::class,'store'])->name('posts.store')->name('vendors.store')->middleware('auth');
 
 Route::get('/posts/{id}', [PostController::class, 'show']);
